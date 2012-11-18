ed36cb2a79f30d87fa14d78c93625cec2757f5b2
Ian Dees <ian.dees@gmail.com>
2012-11-17 21:44:23 -0600

    Keep track of people and their details.

--- ofatechdotorg/main.py
+++ ofatechdotorg/main.py
@@ -1,33 +1,55 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import os
+import logging
+
 import webapp2
 import jinja2
-import os
 
 from google.appengine.api import users
+from google.appengine.ext import db
 
 jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + '/templates'))
+
+class Person(db.Model):
+    user = db.UserProperty()
+    first_name = db.StringProperty()
+    last_name = db.StringProperty()
+    email = db.EmailProperty()
+
+
+    @classmethod
+    def get_by_user(cls, user):
+        return cls.all().filter("user = ", user).get()
+
+    @classmethod
+    def get_for_current_user(cls):
+        return cls.all().filter("user = ", users.get_current_user()).get()
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-    	if users.get_current_user():
+    	user = users.get_current_user()
+    	if user:
             url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
+
+            person = Person.get_by_user(user)
+
+            if person:
+                logging.info('Using person %s' % person)
+                self.redirect('/people')
+                return
+            else:
+                if users.is_current_user_admin():
+                    person = Person(user=user, first_name="", last_name="")
+                    person.put()
+                    logging.info('Adding person for admin user %s.' % user)
+                    self.redirect('/people')
+                    return
+
+                logging.info('No person for user %s' % user)
+                self.redirect(url)
+
         else:
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
@@ -40,6 +62,29 @@ class MainHandler(webapp2.RequestHandler):
         template = jinja_environment.get_template('index.html')
         self.response.out.write(template.render(template_values))
 
+class PeopleHandler(webapp2.RequestHandler):
+    def get(self):
+        person = Person.get_for_current_user()
+
+        if not person:
+            self.redirect('/')
+            return
+
+        url = users.create_logout_url('/')
+        url_linktext = 'Logout'
+
+        people = Person.all().run()
+
+        template_values = {
+            'url': url,
+            'url_linktext': url_linktext,
+            'people': people,
+        }
+
+        template = jinja_environment.get_template('people.html')
+        self.response.out.write(template.render(template_values))
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/people', PeopleHandler)
 ], debug=True)
